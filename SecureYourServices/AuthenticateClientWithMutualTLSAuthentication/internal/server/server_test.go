5bce73faafb971e8928bd0698397132914c8983d
Igor Baiborodine <igor@kiroule.com>
2023-03-03 07:52:55 -0500

    feat: Add source code for "Secure Your Services" chapter - part 4 (#22)

--- SecureYourServices/AuthenticateClientWithMutualTLSAuthentication/internal/server/server_test.go
+++ SecureYourServices/AuthenticateClientWithMutualTLSAuthentication/internal/server/server_test.go
@@ -60,7 +60,9 @@ func setupTest(t *testing.T, fn func(*Config), tls bool) (
 	require.NoError(t, err)
 
 	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
-		CAFile: config.CAFile,
+		CertFile: config.ClientCertFile,
+		KeyFile:  config.ClientKeyFile,
+		CAFile:   config.CAFile,
 	})
 	require.NoError(t, err)
 
@@ -81,6 +83,7 @@ func setupTest(t *testing.T, fn func(*Config), tls bool) (
 		KeyFile:       config.ServerKeyFile,
 		CAFile:        config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server:        true,
 	})
 	require.NoError(t, err)
 	serverCreds := credentials.NewTLS(serverTLSConfig)
